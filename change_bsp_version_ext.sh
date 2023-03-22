236db0e76ffc0035569aa2d1cdd9afa1707691e6
Annemie Potgieter <annemie.potgieter@corigine.com>
2023-03-22 16:34:38 +0200

    Create extensive script for changing BSP veresion
    This bash script installs, updates and flash a BSP version onto a
    specific card using its PCI address. It also reboots the machine to
    ensure the new version is used.
    The script requires the following inputs:
    - last part of path to bsp version e.g. /interim/deb/
    - PCI address e.g. 0000:07:00.0
    - fw file name e.g. nic_AMDA0097-0001_2x40.nffw
    - bsp version e.g. 22.10~00111.main.282e56a-0.bionic_amd64.deb
    Thus, it is suitable for Ubuntu machines. It reloads the driver and
    firmware using a soft reset and flashing the fw to the PCI address.
    
    Signed-off-by: Annemie Potgieter <annemie.potgieter@corigine.com>

---
+++ change_bsp_version_ext.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# $1 input is last part of path to bsp version e.g. /interim/deb/
+# $2 input is PCI address e.g. 0000:07:00.0
+# $3 input is fw file name e.g. nic_AMDA0097-0001_2x40.nffw
+# $4 input is bsp version e.g. 22.10~00111.main.282e56a-0.bionic_amd64.deb
+
+# install BSP version
+apt-get install -y /mnt/cloud/binaries/nfp-bsp/$1nfp-bsp*$4 --allow-downgrades
+
+# ensure interfaces are visible by reloading the driver
+rmmod nfp
+insmod /root/nfp-drv-kmods-private/src/nfp.ko nfp_pf_netdev=0 nfp_dev_cpp=1
+nfp-nsp -Z $2 -R
+nfp-nsp -Z $2 -F /root/agilio-nic-firmware-23.01-0/$3
+rmmod nfp
+insmod /root/nfp-drv-kmods-private/src/nfp.ko nfp_dev_cpp=1
+sleep 2
+ip link
+
+# if ip link shows an interface, continiue (note that grep will run in quiet mode due to -q)
+if ip link | grep -q "np0"; then
+        # flash BSP
+        nfp-fw-update -Z $2 -u -f
+        # if flashed, then reboot
+        if [ $? -eq 0 ]; then
+                sleep 1
+                reboot
+        fi
+fi
